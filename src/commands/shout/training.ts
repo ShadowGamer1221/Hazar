fe7020cbfb11e8a3f84141d17c6000c58311379e
ShadowGamer1221 <85442315+ShadowGamer1221@users.noreply.github.com>
2022-04-11 01:31:17 +0200

    Update training.ts

--- src/commands/shout/training.ts
+++ src/commands/shout/training.ts
@@ -1,20 +1,34 @@
 import { discordClient, robloxClient, robloxGroup } from '../../main';
+import { ButtonInteraction, CommandInteraction, Interaction, Message, MessageActionRow, MessageButton, MessageButtonStyleResolvable, TextChannel } from 'discord.js';
+import { GetGroupRoles } from 'bloxy/src/client/apis/GroupsAPI';
 import { CommandContext } from '../../structures/addons/CommandAddons';
 import { Command } from '../../structures/Command';
+import { MessageEmbed } from 'discord.js';
 import {
-    getUnexpectedErrorEmbed,
-    getSuccessfulShoutEmbed,
     getInvalidRobloxUserEmbed,
-    getSuccessfulTrainingEmbed,
+    getRobloxUserIsNotMemberEmbed,
+    getSuccessfulPromotionEmbed,
+    getUnexpectedErrorEmbed,
+    getNoRankAboveEmbed,
+    getRoleNotFoundEmbed,
+    getVerificationChecksFailedEmbed,
+    getUserSuspendedEmbed,
     checkIconUrl,
+    mainColor,
+    infoIconUrl,
+    xmarkIconUrl,
+    redColor,
     greenColor,
+    getSuccessfulTrainingEmbed,
 } from '../../handlers/locale';
+import { checkActionEligibility } from '../../handlers/verificationChecks';
 import { config } from '../../config';
+import { User, PartialUser, GroupMember } from 'bloxy/dist/structures';
 import { logAction } from '../../handlers/handleLogging';
 import { getLinkedRobloxUser } from '../../handlers/accountLinks';
-import { User, PartialUser, GroupMember } from 'bloxy/dist/structures';
-import { MessageEmbed, TextChannel } from 'discord.js';
-
+import { provider } from '../../database/router';
+import Discord from 'discord.js';
+ 
 class TrainingCommand extends Command {
     constructor() {
         super({
@@ -33,11 +47,8 @@ class TrainingCommand extends Command {
         });
     }
 
+
     async run(ctx: CommandContext) {
-
-        const usermember = ctx.user.id
-        const realmember = await ctx.guild.members.fetch(usermember)
-
         let robloxUser: User | PartialUser;
                 try {
                     const idQuery = ctx.user;
@@ -50,8 +61,21 @@ class TrainingCommand extends Command {
                     return ctx.reply({ embeds: [ getInvalidRobloxUserEmbed() ]});
                 }
 
+        let robloxMember: GroupMember;
         try {
-            await robloxGroup.updateShout(`Greetings! There is a training being hosted by ${robloxUser.name}. Join for a chance of being promoted!\n\n~| GAME LINK: https://www.roblox.com/games/6589778170/Eastside-Cafe-Training-Center?\n\nsigned,\n${robloxUser.name}`);
+            robloxMember = await robloxGroup.getMember(robloxUser.id);
+            if(!robloxMember) throw new Error();
+        } catch (err) {
+            return ctx.reply({ embeds: [ getRobloxUserIsNotMemberEmbed() ]});
+        }
+
+        if(config.database.enabled) {
+            const userData = await provider.findUser(robloxUser.id.toString());
+            if(userData.suspendedUntil) return ctx.reply({ embeds: [ getUserSuspendedEmbed() ] });
+        }
+
+        try {
+            await robloxGroup.updateShout(`Greetings! There is a training being hosted by ${robloxUser.name}. Join for a chance of being promoted!\n\n~| GAME LINK: https://www.roblox.com/games/8897440496/BETA-Eastside-Cafe-Hotels-Resorts\n\nsigned,\n${robloxUser.name}`);
             ctx.reply({ embeds: [ await getSuccessfulTrainingEmbed() ]});
             logAction('Training Announcement', ctx.user);
         } catch (err) {
@@ -59,22 +83,79 @@ class TrainingCommand extends Command {
             return ctx.reply({ embeds: [ getUnexpectedErrorEmbed() ]});
         }
 
-        let channelSend: TextChannel;
-        channelSend = await discordClient.channels.fetch('945372794651283536') as TextChannel;
+        const channelSend = await discordClient.channels.fetch('945372794651283536') as TextChannel;
         console.log(channelSend)
 
-        const successEmbed = new MessageEmbed()
-        .setColor(greenColor)
-        .setDescription(`Greetings! There is a training being hosted by ${robloxUser.name}. Join for a chance of being promoted!\n\n~| GAME LINK: https://www.roblox.com/games/6589778170/Eastside-Cafe-Training-Center?\n\nsigned,\n${robloxUser.name}`)
-        .setTimestamp();
 
-        await channelSend.send({ embeds: [successEmbed] })
+        const sugEmbed = new Discord.MessageEmbed()
+                    .setColor(greenColor)
+                    .setDescription(`Greetings! There is a training being hosted by ${robloxUser.name}. Join for a chance of being promoted!\n\n~| GAME LINK: https://www.roblox.com/games/8897440496/BETA-Eastside-Cafe-Hotels-Resorts\n\nsigned,\n${robloxUser.name}`)
+                    .setTimestamp()
+        
+                const row = new Discord.MessageActionRow().addComponents(
+        
+                    new Discord.MessageButton()
+                        .setCustomId("sug-acc")
+                        .setStyle("DANGER")
+                        .setLabel("Cancel Training"),
 
-        let message = await channelSend.send({
-            content: '<@&945394793796747285>',
-            allowedMentions: { roles: ['945394793796747285'] },
-        });
+                        new Discord.MessageButton()
+                        .setCustomId("sug-accsadadsdsa")
+                        .setStyle("SUCCESS")
+                        .setLabel("End Training"),
+
+                        new Discord.MessageButton()
+                        .setStyle("LINK")
+                        .setLabel("Server")
+                        .setURL("https://www.roblox.com/games/8897440496/BETA-Eastside-Cafe-Hotels-Resorts"),
+        
+                )
+        
+                const sugPage = await channelSend.send({ embeds: [sugEmbed], components: [row] })
+        
+                const col = await sugPage.createMessageComponentCollector({
+                    componentType: "BUTTON"
+                })
+        
+                col.on("collect", async i => {
+        
+                    const interactor = i.guild.members.cache.get(i.user.id)
+        
+                    if (!interactor.roles.cache.some(role => role.name === 'E | Shout Permissions')) return
+        
+                    if (i.customId === "sug-acc") {
+                        
+                        await channelSend.bulkDelete(2)
+
+                        try {
+                            await robloxGroup.updateShout(``);
+                            logAction('Canceled Training', ctx.user);
+                        } catch (err) {
+                            console.log(err);
+                        }
+                    }
+
+                if (i.customId === "sug-accsadadsdsa") {
+        
+                    await channelSend.bulkDelete(2)
+
+                    try {
+                        await robloxGroup.updateShout(``);
+                        logAction('Finished Training', ctx.user);
+                    } catch (err) {
+                        console.log(err);
+                    }
+                }
+
+                })
+
+                let message = await channelSend.send({
+                    content: '<@&945394793796747285>',
+                    allowedMentions: { roles: ['945394793796747285'] },
+                });
+
+                
     }
 }
-
+ 
 export default TrainingCommand;
